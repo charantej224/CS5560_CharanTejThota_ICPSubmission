27967921ce8cde88ad509c2afe82a69710f4a994
charantej224 <charantej.career@gmail.com>
2020-02-16 20:46:44 -0600

    ICP4

--- ICP4/apps/ICP4_Q/question1.py
+++ ICP4/apps/ICP4_Q/question1.py
@@ -9,10 +9,19 @@ rdd = sqlContext.read.csv("data.csv", header=True, inferSchema=True)
 gender_filter = ['Female']
 
 # 1 Filter is transformation and Count is action
+print("----------------------------------------------------------------------------------")
+print("1 Filter is transformation and Count is action")
+print("----------------------------------------------------------------------------------")
 print(rdd.filter(rdd.gender != 'Female').count())
 
 # 2 Distinct is transformation and show is the action
+print("----------------------------------------------------------------------------------")
+print("2 Distinct is transformation and show is the action")
+print("----------------------------------------------------------------------------------")
 return_values = rdd.distinct().show(5)
 
 # 3 groupBy is transformation and count and collect are the action
+print("----------------------------------------------------------------------------------")
+print("3 groupBy is transformation and count and collect are the action")
+print("----------------------------------------------------------------------------------")
 print(rdd.groupBy(rdd.gender).count().collect())
